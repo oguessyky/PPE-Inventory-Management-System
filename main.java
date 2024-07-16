b250b72c794959cee517209130c60589a3873bc9
Oguess <oguessyap@gmail.com>
2024-07-16 21:13:42 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class main {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Hello, World!");
