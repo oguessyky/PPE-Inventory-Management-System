8f4751101778d1d706786fb75ddcdba36e1caf7c
Oguess <oguessyap@gmail.com>
2024-07-16 14:16:18 +0800

    change

--- main.java
+++ main.java
@@ -1,3 +1,11 @@
-public class main {
-    
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Hello, World!");
+        String name = userInput.nextLine();
+        System.out.println(name);
+    }
 }
