1a28718b7f8734fb13159eb064dee9a15a04017e
Oguess <oguessyap@gmail.com>
2024-07-18 09:43:53 +0800

    still trying

--- Main.java
+++ Main.java
@@ -3,8 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (Scanner userInput = new Scanner(System.in)) {
+            String input;
             System.out.print("Input something: ");
-            String input = userInput.nextLine();
+            do { 
+                input = userInput.findInLine("(?=.*Try)(?=.*test).*");
+                if(input == null){
+                    userInput.nextLine();
+                    System.err.println("Something wrong\n");
+                    System.out.print("Input something: ");
+                    
+                }
+            } while (input == null);
             System.out.println("You entered: " + input);
         }
         int i = 0;
--- README.md
+++
@@ -1,3 +0,0 @@
-# OOP assignment
- OOP assignment
-It's not POOP this time dw we got this
