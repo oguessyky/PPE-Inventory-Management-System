58b80903329b86ce1a0b4f8fddd99d8bbd2fa1b4
JosephIsNoob <unidentifiedperson117@gmail.com>
2024-09-12 09:50:02 +0800

    User class foundation

--- Hospital.java
+++ Hospital.java
@@ -1,4 +1,5 @@
 public class Hospital {
-    
-    
+        public Hospital() {
+            
+        }
 }
--- User.java
+++ User.java
@@ -1,3 +1,13 @@
+
+
 public class User {
-    
+
+    public String userID, name, password, userType;
+
+    public User(String userID, String name, String password, String userType) {   
+        this.userID = userID;
+        this.name = name;
+        this.password = password;
+        this.userType = userType;
+    }
 }
